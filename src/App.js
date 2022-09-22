0a21867082f152a6af099fdec4810ca930981e6c
Shigal <shiglaws@gmail.com>
2022-09-22 12:42:44 +0530

    ternary operation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Person = () => {
+  return(
+    <>
+    <h1>First Name: Jack</h1>
+    <h1>Last Name: Sparrow</h1>
+    <h1>Occupation: Pirate</h1>
+    </>
+  );
+}
+
+const App = () => {
+
+  const name = null;
+  const isNameShowing = true;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, { isNameShowing ? name : "someone"}</h1>
+      {name ? 
+    <>
+      test
+    </> :
+    (
+      <>
+    <h1>hello</h1>
+    <h2>tester</h2> 
+    </> 
+    )
+    }
+    
     </div>
   );
 }
