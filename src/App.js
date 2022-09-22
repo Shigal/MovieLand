a9599bdf07a3efb86541a537b6f345a18ec122fa
Shigal <shiglaws@gmail.com>
2022-09-22 14:24:55 +0530

    using useState hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import './App.css';
-
-
-const Person = (props) => {
-  return(
-    <>
-    <h1>First Name: {props.fName}</h1>
-    <h1>Last Name: {props.lName}</h1>
-    <h1>Occupation: {props.job}</h1>
-    </>
-  );
-}
+import {useState} from 'react'
 
 const App = () => {
+
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-     <Person fName='Jane' lName='Maria' job='editor'/>
-     <Person fName='Jack' lName='Sparrow' job='pirate'/>
-     <Person fName='Mark' lName='Antony' job='publisher'/>
+     <button onClick={()=> {setCounter((prevCount) => prevCount - 1)}}>-</button>
+     <h1>{counter}</h1>
+     <button onClick={() => {setCounter((prevCount) => prevCount + 1)}}>+</button>
     </div>
   );
 }
 
-export default App;
+export default App; 
